b6483096b2cace3b197c3744e0e3adb0398c08e0
seondongpyo <psd1390@gmail.com>
2021-03-01 21:58:22 +0900

    Initial commit

---
+++ src/test/java/io/github/seondongpyo/springboard/SpringBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.seondongpyo.springboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

608a9a9194d65d59d56f5d10fd304a4f17b93eaf
seondongpyo <psd1390@gmail.com>
2021-03-07 22:51:49 +0900

    test: Test view controller (/posts, /posts/save)

--- src/test/java/io/github/seondongpyo/springboard/web/controller/ViewControllerTest.java
+++ src/test/java/io/github/seondongpyo/springboard/web/controller/ViewControllerTest.java
@@ -9,7 +9,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class HomeControllerTest {
+class ViewControllerTest {
 
     @Autowired
     private TestRestTemplate testRestTemplate;
@@ -20,5 +20,21 @@ class HomeControllerTest {
         String body = testRestTemplate.getForObject("/", String.class);
 
         assertThat(body).contains("This is a Spring Board Project");
+    }
+
+    @Test
+    @DisplayName("GET '/posts' Request")
+    void posts() {
+        String body = testRestTemplate.getForObject("/posts", String.class);
+
+        assertThat(body).contains("게시물 목록");
+    }
+
+    @Test
+    @DisplayName("GET '/posts/save' Request")
+    void posts_save() {
+        String body = testRestTemplate.getForObject("/posts/save", String.class);
+
+        assertThat(body).contains("게시물 작성");
     }
 }
